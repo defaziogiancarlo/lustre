3130f8a64cc1ef5c4dcad570217c5c8577cca19c
Gian-Carlo DeFazio <defazio1@llnl.gov>
2020-02-11 14:32:20 -0800

    modified test program to have better names

--- lustre/utils/test_param_get_values.c
+++ lustre/utils/test_param_get_values.c
@@ -1,3 +1,29 @@
+/*
+ * GPL HEADER START
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 only,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License version 2 for more details (a copy is included
+ * in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; If not, see
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * GPL HEADER END
+ */
+
+
+
+
+
 #include <lustre/lustreapi.h>
 #include <stdio.h>
 
@@ -30,9 +56,9 @@ void test_pattern(char* pattern)
 int main(int argc, char* argv[])
 {
 	
-	test_single("version");
-	test_single("timeout");
-	test_single("ldlm/namespaces/*/lru_size");
+	test_pattern("version");
+	test_pattern("timeout");
+	test_pattern("ldlm/namespaces/*/lru_size");
 	return 0;
 }
 
