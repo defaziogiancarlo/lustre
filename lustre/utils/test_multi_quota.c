a1d979aee9af1f5442e936c4a9e9088297085f78
Gian-Carlo DeFazio <defazio1@llnl.gov>
2020-03-03 17:01:12 -0800

    test_multi_quota is pipeable

--- lustre/utils/test_multi_quota.c
+++ lustre/utils/test_multi_quota.c
@@ -41,8 +41,48 @@ int mq2(char *fsname)
         return rc;
 } 
 
+int mq2text(char *fsname)
+{
+	int rc = 0;
+	struct if_quotactl qctl;
+	char buf[32];
+	int uid = 0;
+	
+	while (1) {
+		/* initialize qctl for each user */
+		memset(&qctl, 0, sizeof(qctl));
+		qctl.qc_cmd = LUSTRE_Q_GETQUOTA;
+		qctl.qc_type = USRQUOTA;
+		qctl.qc_dqblk.dqb_valid = 1;
+
+		/* get the uid */
+		if (fgets(buf, sizeof(buf), stdin) == NULL) {
+			rc = -errno;
+			break;
+		}
+
+		uid = (unsigned)atoi(buf);
+		qctl.qc_id = uid;
+
+		/* get the quota infomation put into qctl */
+		rc = llapi_quotactl(fsname, &qctl);
+		if (rc < 0) {
+			break;
+		}
+		printf("%u=%s\n", uid, 
+		       ((qctl.qc_dqinfo.dqi_flags >> 2) & 1) ? "True" : "False" );
+
+		//putchar((int)((qctl.qc_dqinfo.dqi_flags >> 2) & 1));
+	}
+
+        return rc;
+} 
+
+
+
+
 // expects fsname on commandline
 int main(int argc, char *argv[])
 {	
-	return mq2(argv[1]);
+	return mq2text(argv[1]);
 }
